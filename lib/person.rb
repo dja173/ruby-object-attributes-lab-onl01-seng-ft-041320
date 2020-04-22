1ea51ae6cec919ff03d6e5163e11f17d0378130f
dustin <djarcene@gmail.com>
2020-04-22 23:14:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,18 +1,17 @@
 class Person
-def name=(name)
-  @name = name
-end
 
-def name
-  @name
-end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
 
-def job=(job)
-  @job = job
-end
+  def job=(job)
+    @job = job
+  end
 
-def job
-  @job
+  def job
+    @job
+  end
 end
-end
-
